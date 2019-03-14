bf52b5d76560abc4f03db526986cff594a964268
alexandrusoare <alexandrusoare98@gmail.com>
2019-03-14 18:18:50 +0200

    haha

--- test/main.js
+++ test/main.js
@@ -4,10 +4,7 @@
     fetch('https://internship-2019.herokuapp.com/api-user-get',
     {
       method: 'POST',
-      credentials: 'include',
       headers: {'Content-Type' : 'application/json'},
-      cache: 'no-cache',
-      mode:'no-cors',
       body:  JSON.stringify(data),
     }).then(console.log("data")).catch(function(error) {
       console.log('There has been a problem with your fetch operation: ' + error.message);
